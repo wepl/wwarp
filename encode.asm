16fcc424ab740061bc9a9b04d85d47f18a4a52da
Bert Jahn <wepl@whdload.de>
2020-05-03 22:54:10 +0200

    encode: optimized encoder using X-flag, input file must be long aligned

--- encode.asm
+++ encode.asm
@@ -7,9 +7,10 @@
 ;		20.02.20 adapted for vamos build
 ;		03.05.20 Blit/S option added which creates MFM suitable for
 ;			 decoding using blitter, first all odd data, then all even
+;		03.05.20 encoding optimized using X-flag
 ;  :Requires.	OS V37+
 ;  :Language.	68000 Assembler
-;  :Translator.	Barfly V2.9
+;  :Translator.	Barfly V2.9, vasm 1.8h
 ;  :To Do.
 ;---------------------------------------------------------------------------*
 ;##########################################################################
@@ -20,7 +21,6 @@
 	INCLUDE	lvo/dos.i
 	INCLUDE	dos/dos.i
 
-	INCLUDE	macros/ntypes.i
 	INCLUDE	macros/sprint.i
 
 ;++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -33,6 +33,9 @@
 		ULONG	gl_rd_input
 		ULONG	gl_rd_output
 		ULONG	gl_rd_blit
+		ULONG	gl_src
+		ULONG	gl_dest
+		ULONG	gl_savelen
 		ULONG	gl_rc			;programs return code
 		ALIGNLONG
 		LABEL	gl_SIZEOF
@@ -42,17 +45,6 @@
 GL	EQUR	A4		;a4 ptr to Globals
 LOC	EQUR	A5		;a5 for local vars
 CPU	=	68000
-
-	IFD BARFLY
-	BOPT	O+			;enable optimizing
-	BOPT	OG+			;enable optimizing
-	BOPT	ODd-			;disable mul optimizing
-	BOPT	ODe-			;disable mul optimizing
-	IFND	.passchk
-	DOSCMD	"WDate  >.date"
-.passchk
-	ENDC
-	ENDC
 
 Version		= 1
 Revision	= 2
@@ -71,7 +63,7 @@ _txt_creator	sprint	"encode ",Version,".",Revision," "
 
 	INCDIR	sources
 	INCLUDE	dosio.i
-		PrintArgs
+		Print
 	INCLUDE	error.i
 		PrintErrorDOS
 	INCLUDE	files.i
@@ -135,22 +127,25 @@ _Start		lea	(_Globals),GL
 		move.l	(gl_rd_input,GL),a0
 		bsr	_LoadFileMsg
 		move.l	d1,d7				;d7 = input length
-		move.l	d0,d6				;d6 = input
+		move.l	d0,(gl_src,GL)			;input
 		beq	.noinput
 
-	;align length
-		addq.l	#3,d7
-		and.b	#%11111100,d7
-
+	;check length
+		moveq	#3,d0
+		and.l	d7,d0
+		beq	.lenok
+		lea	(_badlen),a0
+		bsr	_Print
+		bra	.afterfreedest
+.lenok
 	;alloc destination mem
 		move.l	d7,d0
 		add.l	d0,d0
-		move.l	d0,a3				;a3 = save length
-		addq.l	#4,d0				;+4 security
+		move.l	d0,(gl_savelen,GL)		;save length
 		moveq	#MEMF_ANY,d1
 		move.l	(gl_execbase,GL),a6
 		jsr	(_LVOAllocVec,a6)
-		move.l	d0,d5				;d5 = output
+		move.l	d0,(gl_dest,GL)			;output
 		bne	.memok
 		moveq	#0,d0
 		lea	(_nomem),a0
@@ -163,97 +158,94 @@ _Start		lea	(_Globals),GL
 		bne	.blit
 
 	;create odd, even long by long
-		move.l	d6,a0			;a0 = source data
-		move.l	d5,a1			;a1 = dest mfm
-		clr.l	(a1)+			;skip first long because backfix
-		move.l	#$55555555,d4
+		move.l	(gl_src,GL),a0		;a0 = source data
+		move.l	(gl_dest,GL),a1		;a1 = dest mfm
+		move.l	#$55555555,d4		;d4 = mask even
+		move.l	d4,d5
+		add.l	d5,d5			;d5 = mask odd = $aaaaaaaa
+		moveq	#0,d6			;d6 = previous bit #0
 .loop		move.l	(a0)+,d2		;d2 = odd data
 		move.l	d2,d3			;d3 = even data
 		lsr.l	#1,d2
 		and.l	d4,d2			;mask data bits
 		move.l	d2,d0
-		eor.l	d4,d0			;invert data bits
-		move.l	d0,d1
-		add.l	d0,d0			;left shift inverted data bits
-		lsr.l	#1,d1			;right shift inverted data bits
-		bset	#31,d1			;always set data bit #0 from memory location before
-		and.l	d0,d1			;clock bits = inverted data before & after
+		move.l	d2,d1
+		add.l	d0,d0			;left shift data bits
+		lsr.w	#1,d6			;set X
+		roxr.l	#1,d1			;right shift data bits
+		or.l	d0,d1			;clock bits = data before | after
+		eor.l	d5,d1			;clock bits = not ( data before | after )
 		or.l	d1,d2			;merge clock bits
-		btst	#0,-1(a1)
-		beq	.ok1
-		bclr	#31,d2
-.ok1		move.l	d2,(a1)+		;write odd data
+		move.l	d2,(a1)+		;write odd data
+		lsr.w	#1,d2			;set X
 		and.l	d4,d3			;mask data bits
 		move.l	d3,d0
-		eor.l	d4,d0			;invert data bits
-		move.l	d0,d1
-		add.l	d0,d0			;left shift inverted data bits
-		lsr.l	#1,d1			;right shift inverted data bits
-		bset	#31,d1			;always set data bit #0 from memory location before
-		and.l	d0,d1			;clock bits = inverted data before & after
+		move.l	d3,d1
+		roxr.l	#1,d1			;right shift data bits
+		add.l	d0,d0			;left shift data bits
+		or.l	d0,d1			;clock bits = data before | after
+		eor.l	d5,d1			;clock bits = not ( data before | after )
 		or.l	d1,d3			;merge clock bits
-		btst	#0,-1(a1)
-		beq	.ok2
-		bclr	#31,d3
-.ok2		move.l	d3,(a1)+		;write even data
+		move.l	d3,(a1)+		;write even data
+		move.w	d3,d6
 		subq.l	#4,d7
 		bne	.loop
 		bra	.done
 
 	;create all odd, then all even
-	;suitable for blitter decoding and used in trackdisk.device
-.blit		move.l	d6,a0			;a0 = source data
-		move.l	d5,a1			;a1 = dest mfm odd
+	;suitable for blitter decoding and used in trackdisk.device format
+.blit		move.l	(gl_src,GL),a0		;a0 = source data
+		move.l	(gl_dest,GL),a1		;a1 = dest mfm odd
 		lea	(a1,d7.l),a2		;a2 = dest mfm even
-		clr.l	(a1)+			;skip first long because backfix
-		move.l	#$55555555,d4
+		lea	(a0,d7.l),a3		;a3 = source data end
+		move.l	#$55555555,d4		;d4 = mask even
+		move.l	d4,d5
+		add.l	d5,d5			;d5 = mask odd = $aaaaaaaa
+		moveq	#0,d6			;d6 = previous bit #0 odd
+		move.w	(-2,a3),d7		;last word of data
+		lsr.w	#1,d7			;d7 = previous bit #0 even
 .loopb		move.l	(a0)+,d2		;d2 = odd data
 		move.l	d2,d3			;d3 = even data
 		lsr.l	#1,d2
 		and.l	d4,d2			;mask data bits
 		move.l	d2,d0
-		eor.l	d4,d0			;invert data bits
-		move.l	d0,d1
-		add.l	d0,d0			;left shift inverted data bits
-		lsr.l	#1,d1			;right shift inverted data bits
-		bset	#31,d1			;always set data bit #0 from memory location before
-		and.l	d0,d1			;clock bits = inverted data before & after
+		move.l	d2,d1
+		add.l	d0,d0			;left shift data bits
+		lsr.w	#1,d6			;set X
+		roxr.l	#1,d1			;right shift data bits
+		or.l	d0,d1			;clock bits = data before | after
+		eor.l	d5,d1			;clock bits = not ( data before | after )
 		or.l	d1,d2			;merge clock bits
-		btst	#0,-1(a1)
-		beq	.ok1b
-		bclr	#31,d2
-.ok1b		move.l	d2,(a1)+		;write odd data
+		move.l	d2,(a1)+		;write odd data
+		move.w	d2,d6
 		and.l	d4,d3			;mask data bits
 		move.l	d3,d0
-		eor.l	d4,d0			;invert data bits
-		move.l	d0,d1
-		add.l	d0,d0			;left shift inverted data bits
-		lsr.l	#1,d1			;right shift inverted data bits
-		bset	#31,d1			;always set data bit #0 from memory location before
-		and.l	d0,d1			;clock bits = inverted data before & after
+		move.l	d3,d1
+		add.l	d0,d0			;left shift data bits
+		lsr.w	#1,d7			;set X
+		roxr.l	#1,d1			;right shift data bits
+		or.l	d0,d1			;clock bits = data before | after
+		eor.l	d5,d1			;clock bits = not ( data before | after )
 		or.l	d1,d3			;merge clock bits
-		btst	#0,-1(a2)
-		beq	.ok2b
-		bclr	#31,d3
-.ok2b		move.l	d3,(a2)+		;write even data
-		subq.l	#4,d7
+		move.l	d3,(a2)+		;write odd data
+		move.w	d3,d7
+		cmp.l	a0,a3
 		bne	.loopb
 .done
 	;save file
-		move.l	a3,d0
-		move.l	d5,a0
-		add.l	#4,a0			;skip safety long
+		move.l	(gl_savelen,GL),d0
+		move.l	(gl_dest,GL),a0
 		move.l	(gl_rd_output,GL),a1
 		bsr	_SaveFileMsg
 
 	;free destination memory
-		move.l	d5,a1
+		move.l	(gl_dest,GL),a1
 		move.l	(gl_execbase,GL),a6
 		jsr	(_LVOFreeVec,a6)
 .afterfreedest
 
 	;free input
-		move.l	d6,a1
+		move.l	(gl_src,GL),a1
 		move.l	(gl_execbase,GL),a6
 		jsr	(_LVOFreeVec,a6)
 .noinput
@@ -276,6 +268,7 @@ _badkick	dc.b	"requires Kickstart 2.0 or better.",10,0
 _readargs	dc.b	"read arguments",0
 _nomem		dc.b	"not enough free store",0
 _allocdestmem	dc.b	"alloc temp dest mem",0
+_badlen		dc.b	"file length must be a multiple of 4",10,0
 
 ;subsystems
 _dosname	DOSNAME
